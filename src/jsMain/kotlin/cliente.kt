06f9a0416f28be7269507c92dedc496c73eee764
Luis Carlos de Sousa Menezes <lcsm@ecomp.poli.br>
2019-11-20 20:15:16 -0300

    1o teste no github

---
+++ src/jsMain/kotlin/cliente.kt
@@ -0,0 +1,9 @@
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
+import kotlin.browser.*
+
+
+fun main() {
+    println("ola do cliente")
+
+}
--- src/jvmMain/kotlin/Servidor.kt
+++ src/jvmMain/kotlin/Servidor.kt
@@ -1,5 +1,6 @@
 import io.ktor.application.*
 import io.ktor.features.origin
+import io.ktor.html.respondHtml
 import io.ktor.http.ContentType
 import io.ktor.http.content.*
 import io.ktor.response.respond
@@ -7,13 +8,36 @@ import io.ktor.response.respondText
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.html.body
+import kotlinx.html.br
+import kotlinx.html.title
 
-
+var numero = 0
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         routing {
             get("ola.html") {
-                //PROGRAMA
+                numero++
+                call.respondText("""
+                    <html>
+                    <title>Página dinâmica</title>
+                    <body>
+                    Esta é uma página HTML gerada dinamicamente<br>
+                    Está página já foi gerada $numero de vezes
+                    </body>
+                    </html>
+                """.trimIndent(), ContentType.Text.Html)
+            }
+            get("ola2.html") {
+                numero++
+                call.respondHtml {
+                    title = "Página dinâmica"
+                    body {
+                      +("Esta é uma página HTML gerada dinamicamente")
+                      br
+                      +("esta página já foi gerada $numero de vezes")
+                    }
+                }
             }
             static("static") {
                 files("dados_estaticos")
